e39163d9d63e4ca37c8aa50181ce77e4e7320a95
sadracco <krempcek@gmail.com>
2019-10-17 17:25:44 +0200

    Refactoring, performance measuring

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-import random
-import sys
+from random import randint
 import os
-import numpy as np
 
+import numpy as np
 from PIL import Image, ImageDraw, ImageFile
 
 
@@ -13,23 +11,16 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
 def getArea(x1,x2,x3, y1,y2,y3):
-    return abs((x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2))/2)
+        return abs((x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2))/2)
 
 
-def MakeTriangle(xy1, xy2, xy3, color, im):
-    D = ImageDraw.Draw(im)
-    D.polygon([xy1,xy2,xy3], fill=color)
-    return im
-
-
-def getDiffrence(rgb1, rgb2):
-    return math.sqrt((rgb2[0]-rgb1[0])**2 + (rgb2[1]-rgb1[1])**2 + (rgb2[2]-rgb1[2])**2)
+def makeTriangle(points, color, img):
+    D = ImageDraw.Draw(img)
+    D.polygon(points, fill=color)
 
 
 def score(OrgImg, ScoreImg):
-    w, h = OrgImg.size
     w1, h1 = ScoreImg.size
-
     if w1 != w or h1 != h:
         print("Error at Score funciton\n\n")
         exit()
@@ -37,73 +28,84 @@ def score(OrgImg, ScoreImg):
     pix1=np.array(OrgImg, dtype=np.uint64).reshape((h, w, 3))[:, :, :3]
     pix2 = np.array(ScoreImg, dtype=np.uint64).reshape((h1, w1, 3))
 
-    diff = (np.sqrt(np.square(pix1 - pix2).sum(axis=-1)).sum())
-
-
-
-    return diff
-
-def GetPrevious():
-    return Image.open(FILEDIR+'best.png')
+    return np.sqrt(np.square(pix1 - pix2).sum(axis=-1)).sum()
 
 
 def randPos(w, h):
-    return (random.randint(0, w), random.randint(0, h))
+    return (randint(0, w), randint(0, h))
 
 
 def randColor(preBest=None):
-    if preBest == None or True:
-        return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-    else:
-        return (preBest[3][0] + random.randint(-200,200), preBest[3][1] + random.randint(-200,200),preBest[3][2] + random.randint(-200,200))
+    if not preBest:
+        return (randint(0,255),randint(0,255),randint(0,255))
+    return (preBest[3][0] + randint(-200,200), preBest[3][1] + randint(-200,200),preBest[3][2] + randint(-200,200))
 
 
-def drawOutlined(xy1, xy2, xy3, c, outlineC, thickness, im, name):
-    D = ImageDraw.Draw(im)
-    D.polygon([xy1,xy2,xy3],fill=c)
-    D.line((xy1,xy2),outlineC,thickness)
-    D.line((xy2,xy3),outlineC,thickness)
-    D.line((xy3,xy1),outlineC,thickness)
-    im.save(name)
+def drawOutlined(t, outlineC, thickness, img, name):
+    D = ImageDraw.Draw(img)
+    D.polygon(t[:3],fill=t[3])
+    D.line((t[0],t[1],t[2],t[0]),outlineC,thickness)
+    img.save(name)
 
 
-def condvert(imggg, pixelo):
-    he = pixelo
-    img = Image.open(imggg)
-
-    print("resizing from {}/{}".format(img.size[0],img.size[1]))
+#func returning copy of an image resized to desired heigth (dh) keeping the proportions
+def condvert(img, dh):
     w, h = img.size
-    wpercent = he / float(h)
-    hsize = int(float(w) * float(wpercent))
-    img = img.resize((hsize, he), Image.ANTIALIAS)
-    print("resized to {}/{}".format(img.size[0],img.size[1]))
-    img.save(imggg)
+    print("resizing from {}/{}".format(w,h))
+
+    hRatio = dh/h
+    dw = int(w*hRatio)
+    img = img.resize((dw, dh), Image.ANTIALIAS)
+
+    print("resized to {}/{}".format(dw,dh))
+    return img
+
+
+def inArea(lis, img):
+    for i in lis:
+        try:
+            z = img[i[0],i[1]]
+        except:
+            return False
+    return True
+
+        
+def get(w, h, img):
+    rand1 = randPos(w, h)
+    rand2 = (rand1[0]+randint(-100,100), rand1[1]+randint(-100,100))
+    rand3 = (rand2[0]+randint(-100,100), rand2[1]+randint(-100,100))
+
+    area = getArea(rand1[0],rand1[1],rand2[0],rand2[1],rand3[0],rand3[1])
+
+    if area / (w*h) * 100 > 2  or not inArea([rand1,rand2,rand3], img):
+        return get(w, h, img)
+
+    return (rand1, rand2, rand3)
 
 
 try:
-    condvert(FILEDIR+'Image.png', 160)
-    Real_IM = Image.open(FILEDIR+'Image.png', 'r')
+    Real_IM = condvert(Image.open(FILEDIR+'Image.png', 'r'), 160)
 except FileNotFoundError:
     print('[!] You gotta put a Image.png in this directory!')
     exit()
 
+
 w, h = Real_IM.size
+
+#message below in unnessesary - prints in condvert func say the same thing
 print('> Converted the image to the dimensions of: %s x %s'%(w,h))
 
+
 blank = Image.new('RGB', (w,h), (255,255,255))
-
-
-
 cycles = 0
 generations = 0
 Bests = []
 Scores = []
+imgList = []
 
 try:
-    
     m = Image.open(FILEDIR+'best.png', 'r')
-    w1 , h1 = m.size
-    if w1 != w or h1 != h:
+    if (w,h) != m.size:
         blank.save(FILEDIR+'best.png')
         blank.save(FILEDIR+'gbest.png')
 except FileNotFoundError:
@@ -120,14 +122,12 @@ except FileNotFoundError:
 Draw = ImageDraw.Draw(Best)
 
 accuracy = 5
-if accuracy >= 1000:
+if accuracy > 999:
     print('[!] The accuracy cannot be over 999!')
     exit()
 
 cycleRate = accuracy*2
-colorTry = accuracy
-imgList = []
-
+colorTry = round(accuracy/2.0)
 
 
 while True:
@@ -143,36 +143,10 @@ while True:
     jobs = []
     print('starting gen...')
     for i1 in range(cycleRate):
-        def inArea(lis, img):
-            for i in lis:
-                try:
-                    z = img[i[0],i[1]]
-                except:
-                    return False
-            return True
-                
-        def get(w, h, img):
-            #try:
-            rand1 = randPos(w, h)
-            rand2 = (rand1[0]+random.randint(-100,100), rand1[1]+random.randint(-100,100))
-            rand3 = (rand2[0]+random.randint(-100,100), rand2[1]+random.randint(-100,100))
-
-            area = getArea(rand1[0],rand1[1],rand2[0],rand2[1],rand3[0],rand3[1])
-
-            if ((float(area) / (w*h))*100) > 2  or not inArea([rand1,rand2,rand3], img):
-                return get(w, h, img)
-            else:
-                return (rand1, rand2, rand3)
-            #except:
-                #print(e)
-                #exit()
         
         Norm = Image.open(FILEDIR+'best.png')
 
         rand = get(w,h,GG)
-        rand1 = rand[0]
-        rand2 = rand[1]
-        rand3 = rand[2]
 
         for i2 in range(colorTry):
             if Bests == []:
@@ -182,12 +156,12 @@ while True:
                 zBests.reverse()
                 col = randColor(zBests[0])
             
-            img = MakeTriangle(rand1, rand2, rand3, col, Norm)
-            imgList.append([score(Real_IM,img), (rand1,rand2,rand3,col)])
+            makeTriangle(rand, col, Norm)
+            imgList.append([score(Real_IM, Norm), (rand[0],rand[1],rand[2],col)])
         cycles += 1
         count += 1
 
-    b = 100000000000000000000000000000000000000000000000000000
+    b = 10**62
     b1 = []
 
     for i1 in imgList:
@@ -198,23 +172,23 @@ while True:
     Bests.append(b1)
     
     DrawG = ImageDraw.Draw(GhostBest)
-    DrawG.polygon([b1[0],b1[1],b1[2]], fill=b1[3])
+    DrawG.polygon(b1[:3], fill=b1[3])
     GhostBest.save(FILEDIR+'gbest.png')
     scoreG = score(Real_IM, GhostBest)
 
     if len(Scores) != 0:
         if scoreG <= Scores[len(Scores)-1]:
-            Draw.polygon([b1[0],b1[1],b1[2]], fill=b1[3])
+            Draw.polygon(b1[:3], fill=b1[3])
             Best.save(FILEDIR+'best.png')
         else:
             Best.save(FILEDIR+'best.png')
             print('\n\n--------------------\n\nSkipping and undoing Error --- Image (Undo) Error: %d '%(scoreG))
             continue
     else:
-        Draw.polygon([b1[0],b1[1],b1[2]], fill=b1[3])
+        Draw.polygon(b1[:3], fill=b1[3])
         Best.save(FILEDIR+'best.png')
     
-    drawOutlined(b1[0],b1[1],b1[2],b1[3], (255,0,0), 3, GhostBest, FILEDIR+'gbest.png')
+    drawOutlined(b1, (255,0,0), 3, GhostBest, FILEDIR+'gbest.png')
     GhostBest.save(FILEDIR+'gbest.png')
     imgList = []
     generations += 1
